2000071e0eb905a6b0a1ca85cf18fcf36a524bbb
Ashish Jain <ashishjain0338@gmail.com>
2025-04-20 19:37:11 +0530

    Added basic read-functionality

--- finance_manager/src/components/play/play.js
+++ finance_manager/src/components/play/play.js
@@ -1,7 +1,15 @@
+import { readDataFromExcel } from "../../scripts/data_reader"
+
 function Play(){
+
+    function data_read(){
+        readDataFromExcel("./data/input.xlsx")
+    }
+
     return (
         <div>
             <h1>Hello World</h1>
+            <button onClick={data_read}>CLick Me To read Data</button>
         </div>
     )
 }
--- finance_manager/src/scripts/data_reader.js
+++ finance_manager/src/scripts/data_reader.js
@@ -1,3 +1,27 @@
-function readDataFromExcel(filename){
-    console.log("data-reader Called")
+import * as XLSX from 'xlsx';
+
+function processExcel(wb){
+    // Convert to JSON.
+    const json = XLSX.utils.sheet_to_json(ws);
 }
+
+function readLocalExcel(filepath){
+    fetch(filepath)
+      // Convert to ArrayBuffer.
+      .then((res) => res.arrayBuffer())
+      .then((data) => {
+        const wb = XLSX.read(data, { type: "array" });
+        const wsname = wb.SheetNames[0];
+        console.log("Here2 :", wsname)
+        const ws = wb.Sheets[wsname];
+
+        
+      })
+
+}
+
+export function readDataFromExcel(filepath){
+    console.log("data-reader Called")
+    readLocalExcel(filepath)
+}
+
