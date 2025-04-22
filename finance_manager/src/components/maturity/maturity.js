42bfd54976ac0a9b10ae13893d073961c72100a4
Ashish Jain <ashishjain0338@gmail.com>
2025-04-23 00:15:20 +0530

    Added Time-Period Selector

--- finance_manager/src/components/maturity/maturity.js
+++ finance_manager/src/components/maturity/maturity.js
@@ -2,16 +2,14 @@ import React, { useEffect, useState } from 'react';
 import { Row, Col, Button, Collapse } from "react-bootstrap";
 import { CaretUpFill, CaretDownFill } from '../icons/icons';
 import { MaturitySummary } from './maturity_summary';
+import { MaturityCustom } from './maturity_custom';
 import "./slider-button.css"
 
 function Maturity(props) {
-    const [amountVsFundTypeData, setAmountVsFundTypeData] = useState([[], [], 0])
-    const [amountVsPrimaryHolderData, setAmountVsPrimaryHolderData] = useState([[], [], 0])
-    const [amountVsPrimaryAndSecondaryHolderData, setAmountVsPrimaryAndSecondaryHolderData] = useState([[], [], 0])
-    const [amountVsBankTypeData, setAmountVsBankTypeData] = useState([[], [], 0])
-    const [liquidStats, setLiquidStats] = useState([[], [], 0])
+
 
     const [summarySectionCollapse, setSummarySectionCollapse] = useState(true);
+    const [customSectionCollapse, setCustomSectionCollapse] = useState(true);
     const [isSumMode, setIsSumMode] = useState(false);
     const handleToggle = () => setIsSumMode(!isSumMode);
 
@@ -44,7 +42,6 @@ function Maturity(props) {
 
 
             </Row>
-
             <hr></hr>
             <Collapse in={summarySectionCollapse}>
                 <div>
@@ -55,6 +52,28 @@ function Maturity(props) {
 
             {/* <h3 style={{ padding: "5px" }}>Custom</h3>
             <hr></hr> */}
+            <Row>
+                <Col>
+                    <h3 style={{ padding: "5px" }}>Custom</h3>
+                </Col>
+                <Col>
+                    
+                    <Button className="btn btn-light"
+                        onClick={() => setCustomSectionCollapse(!customSectionCollapse)}
+                        style={{ float: "right" }}
+                    >
+                        {summarySectionCollapse ? <CaretUpFill /> : <CaretDownFill />}
+                    </Button>
+                </Col>
+
+
+            </Row>
+            <hr></hr>
+            <Collapse in={customSectionCollapse}>
+                <div>
+                    <MaturityCustom dbObj={props.dbObj} isSumMode={isSumMode}/>
+                </div>
+            </Collapse>
 
 
         </div>
--- finance_manager/src/components/maturity/maturity_custom.js
+++ finance_manager/src/components/maturity/maturity_custom.js
@@ -1,9 +1,109 @@
-function Maturity_Custom(props){
+import React from 'react';
+import { Form, Row, Col, InputGroup } from 'react-bootstrap';
+import { useState, useEffect } from 'react';
+import { deduceCurrentFinancialYear, getFinancialYear } from '../../scripts/utils';
+import dayjs from 'dayjs';
+
+function MaturityCustom(props) {
+    const [startDate, setStartDate] = useState('');
+    const [endDate, setEndDate] = useState('');
+
+    function changeDate(val, mode){
+        if(mode == "start"){
+            setStartDate(val);
+        }else if(mode == "end"){
+            setEndDate(val);
+        }else if(mode == "both"){
+            setStartDate(val[0])
+            setEndDate(val[1])
+        }
+    }
+
+    function changeDateFromDropdown(val){
+        if(val == "")
+            return;
+        if(val[val.length - 1] == 'y'){
+            // It is financial year
+            var year = parseInt(val.slice(0, val.length - 1))
+            changeDate(getFinancialYear(year), "both")
+            return;
+        }
+
+        const today = dayjs();
+        var todayStr = today.format('YYYY-MM-DD')
+        let cur;
+        switch(val){
+            case "-3m":
+                cur = today.subtract(3, 'month');
+                changeDate([cur.format('YYYY-MM-DD'), todayStr] , "both")
+                break;
+            case "3m":
+                cur = today.add(3, 'month');
+                changeDate([todayStr, cur.format('YYYY-MM-DD')] , "both")
+                break;
+            case "6m":
+                cur = today.add(6, 'month');
+                changeDate([todayStr, cur.format('YYYY-MM-DD')] , "both")
+                break;
+            default:
+
+        }
+
+    }
+
+    function getNextFinancialYearForDropDown(){
+        var currentFY = deduceCurrentFinancialYear()
+        var dropDownOptions = [
+            <option value={(currentFY - 1) + "y"}>F.Y. {currentFY - 1} (Prev)</option>,
+            <option value={currentFY + "y"}>F.Y. {currentFY} (Current)</option>
+        ]
+        for(var i = 0 ; i < 3; i++){
+            var curYear = currentFY + i + 1
+            dropDownOptions.push( <option value={curYear + "y"}>F.Y. {curYear}</option>)
+        }
+        return dropDownOptions
+    }
 
     return (
         <div>
-            <p>Hello World</p>
+            <Form>
+                <Row className="align-items-center">
+                    <Col md={6}>
+                        <Form.Group controlId="startDate">
+
+                            <InputGroup size="mb">
+                                <InputGroup.Text>Time Period: </InputGroup.Text>
+                                <Form.Control
+                                    type="date"
+                                    value={startDate}
+                                onChange={(e) => changeDate(e.target.value, "start")}
+                                />
+                                <InputGroup.Text>To</InputGroup.Text>
+                                <Form.Control
+                                    type="date"
+                                    value={endDate}
+                                onChange={(e) => changeDate(e.target.value, "end")}
+                                />
+
+                            </InputGroup>
+                        </Form.Group>
+                    </Col>
+                    <Col md={3} className="offset-3">
+                        <Form.Group controlId="rangeSelect">
+                            <Form.Select onChange={(e) => changeDateFromDropdown(e.target.value)}>
+                                <option value="">Pre-Defined Ranges</option>
+                                <option value="-3m">Last 3 Months</option>
+                                <option value="3m">Next 3 Months</option>
+                                <option value="6m">Next 6 Months</option>
+                                {getNextFinancialYearForDropDown()}
+                            </Form.Select>
+                        </Form.Group>
+                    </Col>
+                </Row>
+            </Form>
         </div>
     )
 
 }
+
+export { MaturityCustom }
